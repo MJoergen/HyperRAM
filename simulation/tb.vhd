aa84b8b101c0ad7f40407483a12f9b8325bbf566
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2024-10-06 10:45:15 +0200

    simulation/tb.vhd: Beautify

--- simulation/tb.vhd
+++ simulation/tb.vhd
@@ -5,62 +5,62 @@
 -- Created by Michael Jørgensen in 2022 (mjoergen.github.io/HyperRAM).
 
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
 
 entity tb is
 end entity tb;
 
 architecture simulation of tb is
 
-   constant C_DELAY         : time := 1 ns;
-   constant C_CLK_PERIOD    : time := 10 ns; -- 100 MHz
+   constant C_DELAY      : time  := 1 ns;
+   constant C_CLK_PERIOD : time  := 10 ns; -- 100 MHz
 
-   signal sys_clk           : std_logic := '1';
-   signal sys_rstn          : std_logic := '0';
+   signal   sys_clk  : std_logic := '1';
+   signal   sys_rstn : std_logic := '0';
 
-   signal clk               : std_logic;
-   signal clk_del           : std_logic;
-   signal delay_refclk      : std_logic;
-   signal rst               : std_logic;
+   signal   clk          : std_logic;
+   signal   clk_del      : std_logic;
+   signal   delay_refclk : std_logic;
+   signal   rst          : std_logic;
 
-   signal tb_start          : std_logic;
+   signal   tb_start : std_logic;
 
-   signal sys_resetn        : std_logic;
-   signal sys_csn           : std_logic;
-   signal sys_ck            : std_logic;
-   signal sys_rwds          : std_logic;
-   signal sys_dq            : std_logic_vector(7 downto 0);
-   signal sys_rwds_in       : std_logic;
-   signal sys_dq_in         : std_logic_vector(7 downto 0);
-   signal sys_rwds_out      : std_logic;
-   signal sys_dq_out        : std_logic_vector(7 downto 0);
-   signal sys_rwds_oe_n     : std_logic;
-   signal sys_dq_oe_n       : std_logic_vector(7 downto 0);
+   signal   sys_resetn    : std_logic;
+   signal   sys_csn       : std_logic;
+   signal   sys_ck        : std_logic;
+   signal   sys_rwds      : std_logic;
+   signal   sys_dq        : std_logic_vector(7 downto 0);
+   signal   sys_rwds_in   : std_logic;
+   signal   sys_dq_in     : std_logic_vector(7 downto 0);
+   signal   sys_rwds_out  : std_logic;
+   signal   sys_dq_out    : std_logic_vector(7 downto 0);
+   signal   sys_rwds_oe_n : std_logic;
+   signal   sys_dq_oe_n   : std_logic_vector(7 downto 0);
 
    -- HyperRAM simulation device interface
-   signal hr_resetn         : std_logic;
-   signal hr_csn            : std_logic;
-   signal hr_ck             : std_logic;
-   signal hr_rwds           : std_logic;
-   signal hr_dq             : std_logic_vector(7 downto 0);
+   signal   hr_resetn : std_logic;
+   signal   hr_csn    : std_logic;
+   signal   hr_ck     : std_logic;
+   signal   hr_rwds   : std_logic;
+   signal   hr_dq     : std_logic_vector(7 downto 0);
 
 
    component s27kl0642 is
       port (
-         DQ7      : inout std_logic;
-         DQ6      : inout std_logic;
-         DQ5      : inout std_logic;
-         DQ4      : inout std_logic;
-         DQ3      : inout std_logic;
-         DQ2      : inout std_logic;
-         DQ1      : inout std_logic;
-         DQ0      : inout std_logic;
-         RWDS     : inout std_logic;
-         CSNeg    : in    std_logic;
-         CK       : in    std_logic;
-         CKn      : in    std_logic;
-         RESETNeg : in    std_logic
+         dq7      : inout std_logic;
+         dq6      : inout std_logic;
+         dq5      : inout std_logic;
+         dq4      : inout std_logic;
+         dq3      : inout std_logic;
+         dq2      : inout std_logic;
+         dq1      : inout std_logic;
+         dq0      : inout std_logic;
+         rwds     : inout std_logic;
+         csneg    : in    std_logic;
+         ck       : in    std_logic;
+         ckn      : in    std_logic;
+         resetneg : in    std_logic
       );
    end component s27kl0642;
 
@@ -70,7 +70,7 @@ begin
    -- Generate clock and reset
    ---------------------------------------------------------
 
-   sys_clk  <= not sys_clk after C_CLK_PERIOD/2;
+   sys_clk  <= not sys_clk after C_CLK_PERIOD / 2;
    sys_rstn <= '0', '1' after 100 * C_CLK_PERIOD;
 
    clk_controller_inst : entity work.clk_controller
@@ -88,7 +88,7 @@ begin
    -- Generate start signal for trafic generator
    --------------------------------------------------------
 
-   p_tb_start : process
+   tb_start_proc : process
    begin
       tb_start <= '0';
       wait for 160 us;
@@ -97,18 +97,18 @@ begin
       wait until clk = '1';
       tb_start <= '0';
       wait;
-   end process p_tb_start;
+   end process tb_start_proc;
 
 
    --------------------------------------------------------
    -- Instantiate core test generator
    --------------------------------------------------------
 
-   i_core_wrapper : entity work.core_wrapper
+   core_wrapper_inst : entity work.core_wrapper
       generic map (
-         G_SYS_ADDRESS_SIZE  => 6,
-         G_ADDRESS_SIZE      => 20,
-         G_DATA_SIZE         => 64
+         G_SYS_ADDRESS_SIZE => 6,
+         G_ADDRESS_SIZE     => 20,
+         G_DATA_SIZE        => 64
       )
       port map (
          clk_i           => clk,
@@ -131,18 +131,18 @@ begin
          hr_dq_in_i      => sys_dq_in,
          hr_dq_out_o     => sys_dq_out,
          hr_dq_oe_n_o    => sys_dq_oe_n
-      ); -- i_core_wrapper
+      ); -- core_wrapper_inst
 
    ----------------------------------
    -- Tri-state buffers for HyperRAM
    ----------------------------------
 
-   sys_rwds                 <= sys_rwds_out when sys_rwds_oe_n = '0' else
-                                 'Z';
+   sys_rwds <= sys_rwds_out when sys_rwds_oe_n = '0' else
+               'Z';
 
    sys_dq_gen : for i in 0 to 7 generate
       sys_dq(i) <= sys_dq_out(i) when sys_dq_oe_n(i) = '0' else
-                     'Z';
+                   'Z';
    end generate sys_dq_gen;
 
    sys_rwds_in <= sys_rwds;
@@ -153,51 +153,53 @@ begin
    -- Connect controller to device (with delay)
    ---------------------------------------------------------
 
-   hr_resetn <= sys_resetn after C_DELAY;
-   hr_csn    <= sys_csn    after C_DELAY;
-   hr_ck     <= sys_ck     after C_DELAY;
+   hr_resetn   <= sys_resetn after C_DELAY;
+   hr_csn      <= sys_csn    after C_DELAY;
+   hr_ck       <= sys_ck     after C_DELAY;
 
-   i_wiredelay2_rwds : entity work.wiredelay2
+   wiredelay2_rwds_inst : entity work.wiredelay2
       generic map (
          G_DELAY => C_DELAY
       )
       port map (
-         A => sys_rwds,
-         B => hr_rwds
-      );
+         a => sys_rwds,
+         b => hr_rwds
+      ); -- wiredelay2_rwds_inst
 
-   gen_dq_delay : for i in 0 to 7 generate
-   i_wiredelay2_rwds : entity work.wiredelay2
-      generic map (
-         G_DELAY => C_DELAY
-      )
-      port map (
-         A => sys_dq(i),
-         B => hr_dq(i)
-      );
-   end generate gen_dq_delay;
+   dq_delay_gen : for i in 0 to 7 generate
+
+      wiredelay2_rwds_inst : entity work.wiredelay2
+         generic map (
+            G_DELAY => C_DELAY
+         )
+         port map (
+            a => sys_dq(i),
+            b => hr_dq(i)
+         ); -- wiredelay2_rwds_inst
+
+   end generate dq_delay_gen;
 
 
    ---------------------------------------------------------
    -- Instantiate HyperRAM simulation model
    ---------------------------------------------------------
 
-   i_s27kl0642 : s27kl0642
+   s27kl0642_inst : component s27kl0642
       port map (
-         DQ7      => hr_dq(7),
-         DQ6      => hr_dq(6),
-         DQ5      => hr_dq(5),
-         DQ4      => hr_dq(4),
-         DQ3      => hr_dq(3),
-         DQ2      => hr_dq(2),
-         DQ1      => hr_dq(1),
-         DQ0      => hr_dq(0),
-         RWDS     => hr_rwds,
-         CSNeg    => hr_csn,
-         CK       => hr_ck,
-         CKn      => not hr_ck,
-         RESETNeg => hr_resetn
-      ); -- i_s27kl0642
+         dq7      => hr_dq(7),
+         dq6      => hr_dq(6),
+         dq5      => hr_dq(5),
+         dq4      => hr_dq(4),
+         dq3      => hr_dq(3),
+         dq2      => hr_dq(2),
+         dq1      => hr_dq(1),
+         dq0      => hr_dq(0),
+         rwds     => hr_rwds,
+         csneg    => hr_csn,
+         ck       => hr_ck,
+         ckn      => not hr_ck,
+         resetneg => hr_resetn
+      ); -- s27kl0642_inst
 
 end architecture simulation;
 
